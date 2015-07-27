eb308184010bb7da4b24779140d4b0eb60fc82ca
bostar <starbo.net@gmail.com>
2015-07-27 15:09:38 +0800

    add cmd plus local address

--- Projects/ble/SimpleBLEPeripheral/Source/zlgBeeApp/zlg_protocol.c
+++ Projects/ble/SimpleBLEPeripheral/Source/zlgBeeApp/zlg_protocol.c
@@ -31,38 +31,42 @@ void ackLinkTest(unsigned char *IEEEAddress)
 
 void dateRequset(void)
 {
-    char wbuf[4];
+    char wbuf[6];
     wbuf[0] = 'C';
     wbuf[1] = 'F';
     wbuf[2] = 'G';
     wbuf[3] = cmdDataRequest;//cmd  
-//    wbuf[4] = stDevInfo->devLoacalNetAddr[0];
-//    wbuf[5] = stDevInfo->devLoacalNetAddr[1];
+    wbuf[4] = stDevInfo->devLoacalNetAddr[0];
+    wbuf[5] = stDevInfo->devLoacalNetAddr[1];
     
-    NPI_WriteTransport( (unsigned char *)wbuf , 4 );
+    NPI_WriteTransport( (unsigned char *)wbuf , 6 );
 }
 
 void eventReport(parkingEvent_t event)
 {
-    char wbuf[5];
+    char wbuf[7];
     wbuf[0] = 'S';
     wbuf[1] = 'E';
     wbuf[2] = 'N';
     wbuf[3] = cmdEventReport;
     wbuf[4] = event;
+    wbuf[5] = stDevInfo->devLoacalNetAddr[0];
+    wbuf[6] = stDevInfo->devLoacalNetAddr[1];
     
-    NPI_WriteTransport( (unsigned char *)wbuf , 5 );
+    NPI_WriteTransport( (unsigned char *)wbuf , 7 );
 }
 
 void batteryRemainingReport(unsigned char voltage)
 {
-    char wbuf[5];
+    char wbuf[7];
     
     wbuf[0] = 'S';
     wbuf[1] = 'E';
     wbuf[2] = 'N';
     wbuf[3] = cmdBatteryRemainReport;
     wbuf[4] = voltage;
+    wbuf[5] = stDevInfo->devLoacalNetAddr[0];
+    wbuf[6] = stDevInfo->devLoacalNetAddr[1];
     
-    NPI_WriteTransport( (unsigned char *)wbuf , 5 );
+    NPI_WriteTransport( (unsigned char *)wbuf , 7 );
 }
